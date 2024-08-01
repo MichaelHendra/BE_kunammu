343eaefe640944fadf04ccf2a8ebb9bf6d97cf55
MichaelHendra <michaelhendranata01@gmail.com>
2024-08-01 10:42:43 +0700

    Regiter User And Login Implemet JWT

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'username' => 'required|string|max:255|unique:users,username',
+            'password' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'displayName' => 'required|string|max:255',
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+        $validateData = $validator->validated();
+        $validateData['saldo'] = '0';
+        $validateData['image'] = 'image';
+
+        $data = User::create($validateData);
+
+        return response()->json($data,201);
+    }
+
+    public function login(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        $credentials = $request->only('username', 'password');
+
+        if(!$token = auth()->guard('api')->attempt($credentials)){
+            return response()->json([
+                'success' => false,
+                'message' => 'Email atau Password Anda Salah'
+            ], 401);
+        }
+        return response()->json([
+            'success' => true,
+            'user'    => auth()->guard('api')->user(),
+            'token'   => $token
+        ], 200);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
 
 Route::get('/barang', [BarangController::class, 'index']);
 Route::get('/barang/show/{id}', [BarangController::class, 'show']);
